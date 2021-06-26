11656215dbc375961cf90a5779c99f0c769c43b7
Fiky Anggara <71414823+fiky-web@users.noreply.github.com>
2021-06-27 02:30:47 +0700

    add titip

---
+++ TitipAnak.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <windows.h>
+#include <conio.h>
+#include <fstream>
+#include <time.h>
+#include "admin.h"
+#include "titipanak.h"
+using namespace std;
+titipanak::titipanak(){
+}
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+void titipanak::jadwal(){
+ifstream jadwal("jadwal.txt");
+while(jadwal){
+jadwal.getline(buffer,1000);
+cout<<buffer<<endl;
+}
+jadwal.close();
+}
+//---------------------------------------------------------------------------
---
+++ TitipAnak.h
@@ -0,0 +1,15 @@
+#ifndef TITIPANAK_H_INCLUDED
+#define TITIPANAK_H_INCLUDED
+
+class titipanak : public admin
+{
+public:
+
+titipanak();
+void jadwal();
+
+};
+
+
+
+#endif // TITIPANAK_H_INCLUDED
